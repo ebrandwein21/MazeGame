08ce4e3dbd77ce185fafc8c451da0da338f8cdc2
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-04-19 18:19:55 -0500

    added cell components enum

--- src/hw4/maze/build/CellComponents.java
+++ src/hw4/maze/build/CellComponents.java
@@ -1,36 +1,10 @@
 package hw4.maze.build;
 
-public class CellComponents{
-
-    public CellComponents Apperture;
-    public CellComponents Exit;
-    public CellComponents Wall;
-    
-    
-    public CellComponents getApperture() {
-		return Apperture;
- 	}
+public enum CellComponents{
 	
-	public void setSize(CellComponents Apperture) {
-		this.Apperture = Apperture;
+		APERTURE,
+		WALL,
+		EXIT
+   
 	}
 	
-	 
-    public CellComponents getExit() {
-		return Exit;
- 	}
-	
-	public void setExit(CellComponents Exit) {
-		this.Exit = Exit;
-	}
-	
-	 
-    public CellComponents getWall() {
-		return Wall;
- 	}
-	
-	public void setWall(CellComponents Wall) {
-		this.Wall = Wall;
-	}
-	
-}
