7a6d89387abb264d13cd1a370f478ae89461ce52
ebrandwein21 <150644846+ebrandwein21@users.noreply.github.com>
2025-04-19 18:57:51 -0500

    added cell constructor

--- src/hw4/maze/build/Cell.java
+++ src/hw4/maze/build/Cell.java
@@ -4,6 +4,16 @@ public class Cell{
 	
 	//methods are tentative and may need parameters, different access levels etc 
 	
+	CellComponents cellComponents;
+	
+	public Cell(CellComponents cellComponents, CellComponents cellComponents2,CellComponents cellComponents3, CellComponents cellComponents4 )
+	{
+		this.cellComponents = cellComponents; 
+		this.cellComponents = cellComponents2;
+		this.cellComponents = cellComponents3;
+		this.cellComponents = cellComponents4;
+	}
+	
 	public void assertsEquals()
 	{
 		
